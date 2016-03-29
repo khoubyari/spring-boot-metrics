d6e1cccf61d86a7b4602124c06c5256ce4710b6d
Siamak Khoubyari <skhoubyari@xactlycorp.com>
2016-03-29 09:46:51 -0700

    added new integration code

--- src/main/java/com/khoubyari/example/Application.java
+++ src/main/java/com/khoubyari/example/Application.java
@@ -3,12 +3,16 @@ package com.khoubyari.example;
 
 import ch.qos.logback.classic.Logger;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
+import org.springframework.boot.actuate.metrics.writer.MessageChannelMetricWriter;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHandler;
 
 @EnableAutoConfiguration  // Sprint Boot Auto Configuration
@@ -27,9 +31,20 @@ public class Application extends SpringBootServletInitializer {
     }
 
     @Bean
+    public MessageChannel metricsChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ExportMetricWriter
+    public MessageChannelMetricWriter messageChannelMetricWriter() {
+        return new MessageChannelMetricWriter(metricsChannel());
+    }
+
+    @Bean
     @ServiceActivator(inputChannel = "metricsChannel")
     public MessageHandler metricsHandler() {
-        System.out.println(">>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<"); return System.out::println;
+        return System.out::println;
     }
 
 }
